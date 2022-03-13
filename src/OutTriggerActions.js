946515d2252edbb37d2813156898ebb36c1dd21b
sascanagl <carl.nagle@sas.com>
2022-03-13 00:20:48 -0500

    Moved and fully integrated into DialogServer

--- src/OutTriggerActions.js
+++
@@ -1,9 +0,0 @@
-
-class OutTriggerActions {
-    constructor(props){
-        this.message = props.message ?? "";
-        this.trigger = props.trigger ?? "";
-        this.actions = props.actions ?? "";
-    }
-}
-export default OutTriggerActions;
